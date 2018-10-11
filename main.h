ca494919129d58ffe1433604604c1ba11fd5c9cc
Elias Santos <elias.formula@gmail.com>
2018-10-11 11:52:28 -0300

    Add files

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
 #include <16F877A.h>
-#device adc=8
+#device adc=16
 
 #FUSES NOWDT                 	//No Watch Dog Timer
 #FUSES XT                    	//Crystal osc <= 4mhz
@@ -9,7 +9,7 @@
 #FUSES NOBROWNOUT            	//No brownout reset
 #FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 #FUSES NOCPD                 	//No EE protection
-#FUSES NOWRT                 	//Program memory not write protected
+#FUSES WRT_50%               	//Lower half of Program Memory is Write Protected
 
 #use delay(clock=4000000)
 
