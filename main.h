7144974f6973c40c783b1bbcc11705525086b4bf
Elias Santos <elias.formula@gmail.com>
2018-10-11 11:57:15 -0300

    add files

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 #include <16F877A.h>
-#device adc=16
+#device adc=8
 
 #FUSES NOWDT                 	//No Watch Dog Timer
-#FUSES XT                    	//Crystal osc <= 4mhz
+#FUSES HS                    	//High speed Osc (> 4mhz)
 #FUSES NOPUT                 	//No Power Up Timer
 #FUSES NOPROTECT             	//Code not protected from reading
 #FUSES NODEBUG               	//No Debug mode for ICD
